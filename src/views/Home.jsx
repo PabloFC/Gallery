19eaf7f3a14fa504cd6b32ec95155b5ba0895f71
PabloFC <pablofc_68@hotmail.com>
2024-04-09 19:44:45 +0200

    v2

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import { useGaleryContext } from "../contexts/GaleryContext";
-
 
 const Home = () => {
     const { gallery, setGallery } = useGaleryContext();
+    const [category, setCategory] = useState("todos");
+
+    const filterGallery = category === "todos" ? gallery : gallery.filter(photo => photo.category === category);
     
 
     const changeCategory = (category) => {
-		const filterGalery = gallery.filter(photo => photo.category === category);
-        setGallery(filterGalery);
+        setCategory(category);
+		// const filterGalery = gallery.filter(photo => photo.category === category);
+        // setGallery(filterGalery);
 	};
 
   return (
@@ -17,9 +21,11 @@ const Home = () => {
         <button onClick={() => changeCategory("perros")}>Perros</button>
         <button onClick={() => changeCategory("gatos")}>Gatos</button>
         <button onClick={() => changeCategory("conejos")}>Conejos</button>
+        <button onClick={() => changeCategory("todos")}>Todos</button>
+        
       </div>
       <div>
-        {gallery.map((photo, index) => (
+        {filterGallery.map((photo, index) => (
           <img key={index} src={photo.img} alt={`Imagen ${index}`} />
         ))}
       </div>
